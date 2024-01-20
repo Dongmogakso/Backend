7d883e85ca2531259f2b70666a0e51bb3ce32bb4
yjoon2k <syjoon2k@gmail.com>
2024-01-20 15:13:24 +0900

    feat: auth, stores, schedules template added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { StoresController } from './stores/stores.controller';
+import { StoresModule } from './stores/stores.module';
+import { SchedulesController } from './schedules/schedules.controller';
+import { SchedulesModule } from './schedules/schedules.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, StoresModule, SchedulesModule],
+  controllers: [AppController, AuthController, StoresController, SchedulesController],
   providers: [AppService],
 })
 export class AppModule {}
